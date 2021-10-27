96f2cffc8f5cdaf4894d8d0408ddbf894d0ec8af
YunYoonBin <yubiny287@gmail.com>
2021-10-27 21:01:19 +0900

    React

--- 2장 JSX 문법/조건부 연산자/src/App.js
+++ 2장 JSX 문법/조건부 연산자/src/App.js
@@ -1,19 +1,22 @@
 
 
 function App() {
-  
-  const name = "조건부 연산자"
-  
-  return (
-    <div className="App">
-  
-      {name === "조건부 연산자" 
-      ? ( <h1>참</h1> ) 
-      : ( <h1>거짓</h1> )
-      }
 
-    </div>
-  );
+  const name = "리앣트"
+
+  return (
+    
+    <div>
+  
+  {
+  name === "리액트"
+    ? ( <h1>참</h1> )
+    : ( <h1>거짓</h1> )
+  }
+
+    </div> 
+
+  )
 }
 
 export default App;
