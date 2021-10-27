07e0ad0d37fd1e79350671d70236750ee4da24c5
YunYoonBin <yubiny287@gmail.com>
2021-10-28 00:25:05 +0900

    React

---
+++ 3장_컴포넌트/클래스형 컴포넌트/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    const name = "테스트"
+
+    return <div className="react">{name}</div>
+  }  
+}
+
+export default App;
