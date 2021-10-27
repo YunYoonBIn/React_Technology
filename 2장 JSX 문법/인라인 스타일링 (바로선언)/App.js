7621d79ff4bc09467925266f6d833d144baa2617
YunYoonBin <yubiny287@gmail.com>
2021-10-27 22:52:37 +0900

    React

---
+++ 2장 JSX 문법/인라인 스타일링 (바로선언)/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+
+  const 테스트 = "test"
+
+
+  return (
+
+    <div style={{
+      //객체 형태로 스타일링
+      backgroundColor: 'black',
+      color: 'aqua',
+      fontSize: '48px',
+      fontWeight: 'bold',
+      padding: 16,
+    }}>
+
+      {테스트}
+
+    </div>
+  )
+}
+
+export default App;
