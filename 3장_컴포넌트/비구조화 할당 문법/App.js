867558ea76f8ac1bc65947445475814ea0f12d2a
YunYoonBin <yubiny287@gmail.com>
2021-10-29 00:20:01 +0900

    React

---
+++ 3장_컴포넌트/비구조화 할당 문법/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import MyComponent from "./MyComponent";
+
+const App = () => {
+  return <MyComponent>TEST</MyComponent>;
+};
+
+export default App;
---
+++ 3장_컴포넌트/비구조화 할당 문법/MyComponent.js
@@ -0,0 +1,12 @@
+const MyComponent = ( { name, children} ) => {
+ 
+  return <div>안녕하세요 제 이름은 {name} 입니다. 
+  <br />
+  children 값은 {children}입니다.
+  </div>;
+};
+
+MyComponent.defaultProps = {
+  name : "Test",
+}
+export default MyComponent;
