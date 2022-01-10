aa28d0b7442e394f1573f0a4ecad8666f5a4a50c
YunYoonBin <yubiny287@gmail.com>
2022-01-10 20:14:33 +0900

    react

---
+++ 5장_ref/예제 컴포넌트/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import ValidationSample from "./ValidationSample";
+
+const App = () => {
+  return (
+    <div>
+      <ValidationSample />
+    </div >
+  )
+}
+
+export default App;
