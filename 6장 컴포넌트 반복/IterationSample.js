9abe5b142b86fb731d4931a9975e9c45ba99abc9
YunYoonBin <yubiny287@gmail.com>
2022-01-20 23:15:33 +0900

    react

--- 6장 컴포넌트 반복/IterationSample.js
+++ 6장 컴포넌트 반복/IterationSample.js
@@ -1,11 +1,12 @@
 const IterationSample = () => {
-    const 과일목록 = ['사과', '파인애플', '멜론', '바나나']
-    const 과일리스트 = 과일목록.map(과일들 => <li>{과일들}</li>)
+    var numbers = [1, 2, 3, 4]
+    var processed = numbers.map(function (num) {
+        return num * num
+    })
+    console.log(processed)
     return (
         <div>
-            <ul>
-                {과일리스트}
-            </ul>
+
         </div>
     )
 }
