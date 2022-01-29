3fe7da737f1140e5e4cb3c23c3f545f11438d534
YunYoonBin <yubiny287@gmail.com>
2022-01-29 19:59:11 +0900

    recat

---
+++ 8장 Hooks/useState/useState여러 번 사용/Hooks.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const Hooks = () => {
+    const [name, setName] = useState('');
+    const [nickName, setNickName] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+    const onChangeNickName = e => {
+        setNickName(e.target.value);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={name}
+                onChange={onChangeName} />
+            <input
+                type="text"
+                value={nickName}
+                onChange={onChangeNickName} />
+
+            <p>이름: {name}</p>
+            <p>닉네임: {nickName}</p>
+
+        </div>
+    )
+}
+
+export default Hooks
