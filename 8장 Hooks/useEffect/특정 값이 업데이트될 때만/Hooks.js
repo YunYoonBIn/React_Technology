ae1c391b4a0313dc783883e29282c3af4e7a1e33
YunYoonBin <yubiny287@gmail.com>
2022-01-30 20:55:40 +0900

    react

---
+++ 8장 Hooks/useEffect/특정 값이 업데이트될 때만/Hooks.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+const Hooks = () => {
+
+    const [name, setName] = useState('');
+    const [nickName, setNickName] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+    const onChangeNickName = e => {
+        setNickName(e.target.value);
+    };
+
+    useEffect(() => {
+        console.log(name)
+    }, [name])
+
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={name}
+                onChange={onChangeName} />
+            <input
+                type="text"
+                value={nickName}
+                onChange={onChangeNickName} />
+
+            <p>이름: {name}</p>
+            <p>닉네임: {nickName}</p>
+
+        </div>
+    )
+}
+
+export default Hooks
